c276a73926daa4c3d71589947c9314f5ecac9bc2
Jason Schousboe <questionmuppet@gmail.com>
2020-11-21 15:24:33 -0600

    Wrote ArgumentParser wrapper for GetOpt

---
+++ src/Arguments/ArgumentParser.php
@@ -0,0 +1,231 @@
+<?php
+/**
+ * ArgumentParser
+ * 
+ * Parses command-line options and arguments
+ */
+
+namespace Wp_Dev_Tools\Arguments;
+
+use GetOpt\GetOpt;
+use GetOpt\Option;
+use GetOpt\ArgumentException;
+use GetOpt\ArgumentException\Invalid;
+
+final class ArgumentParser
+{
+    /**
+     * Error codes
+     */
+    const SYNTAX_ERROR = 1;
+    const INVALID_ARGUMENT = 2;
+
+    /**
+     * Output
+     */
+    private string $message = '';
+    private int $error = 0;
+
+    /**
+     * Settings
+     */
+    private array $settings = [
+        'script' => '',
+        'version' => '',
+        'strict_operands' => true,
+    ];
+
+    /**
+     * GetOpt library class
+     */
+    private GetOpt $getopt;
+
+    /**
+     * Constructor
+     */
+    public function __construct(array $args, array $settings = [])
+    {
+        // Parser settings
+        $this->set_settings($settings);
+
+        // Extract argument definitions
+        $options = $this->collate_options($args['options'] ?? []);
+        $operands = $args['operands'] ?? [];
+
+        // Invoke GetOpt
+        $this->getopt = new GetOpt($options, $this->getopt_settings());
+        $this->getopt->addOperands($operands);
+    }
+
+    /**
+     * Set parser settings
+     */
+    private function set_settings(array $settings): void
+    {
+        foreach ($settings as $key => $value)
+        {
+            array_key_exists($key, $this->settings) && $this->settings[$key] = $value;
+        }
+    }
+
+    /**
+     * Settings used to instantiate GetOpt object
+     */
+    private function getopt_settings(): array
+    {
+        return [
+            GetOpt::SETTING_STRICT_OPERANDS => $this->settings['strict_operands'],
+        ];
+    }
+
+    /**
+     * -----------------
+     *   O U T P U T S
+     * -----------------
+     */
+
+    /**
+     * Check for output generated during parsing
+     */
+    public function has_output(): bool
+    {
+        return strlen($this->output());
+    }
+
+    /**
+     * Get the parse message (error or info)
+     * 
+     * @return string Message generated during the parse, or empty string
+     */
+    public function output(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Get error code
+     * 
+     * @return int Error code if encountered, 0 for successful parsing
+     */
+    public function error_code(): int
+    {
+        return $this->error;
+    }
+
+    /**
+     * Get an option value
+     * 
+     * @return mixed
+     */
+    public function option(string $key)
+    {
+        return $this->getopt->getOption($key);
+    }
+
+    /**
+     * Get a positional parameter value
+     * 
+     * @return mixed
+     */
+    public function operand(string $key)
+    {
+        return $this->getopt->getOperand($key);
+    }
+
+    /**
+     * -------------
+     *   P A R S E
+     * -------------
+     */
+
+    /**
+     * Parse command-line arguments
+     * 
+     * @param mixed $arguments
+     */
+    public function parse($arguments = null): void
+    {
+        try
+        {
+            $this->getopt->process($arguments);
+        }
+        catch (ArgumentException $e)
+        {
+            if (!$this->has_information_message())
+            {
+                error_log($e->getMessage() . PHP_EOL);
+                $this->message = PHP_EOL . $this->getopt->getHelpText();
+                $this->error = $e instanceof Invalid ? self::INVALID_ARGUMENT : self::SYNTAX_ERROR;
+            }
+        }
+        finally
+        {
+            $this->message .= $this->information_message();
+        }
+    }
+
+    /**
+     * Check for a non-error information message
+     */
+    private function has_information_message(): bool
+    {
+        return $this->option('help') || $this->option('version');
+    }
+
+    /**
+     * Get the output for an information command
+     */
+    private function information_message(): string
+    {
+        if ($this->option('help')) {
+            return $this->getopt->getHelpText();
+        }
+        if ($this->option('version')) {
+            return $this->version_message();
+        }
+        return '';
+    }
+    
+    /**
+     * Get version message
+     */
+    private function version_message(): string
+    {
+        return sprintf(
+            "%s: v%s" . PHP_EOL,
+            $this->settings['script'],
+            $this->settings['version']
+        );
+    }
+
+    /**
+     * -----------------
+     *   O P T I O N S
+     * -----------------
+     */
+
+    /**
+     * Collate common and user-provided options and sort
+     * 
+     * @return Option[]
+     */
+    public function collate_options(array $user_opts): array
+    {
+        $opts = array_replace($this->common_opts(), $user_opts);
+        ksort($opts);
+        return $opts;
+    }
+
+    /**
+     * Options common to all scripts
+     * 
+     * @return Option[]
+     */
+    private function common_opts(): array
+    {
+        return [
+            'h' => Option::create('h', 'help')->setDescription('Show this help and quit'),
+            'v' => Option::create('v', 'version')->setDescription('Show version information and quit'),
+        ];
+    }
+}
---
+++ tests/Unit/Arguments/ArgumentParser_Test.php
@@ -0,0 +1,236 @@
+<?php
+declare(strict_types=1);
+
+namespace Wp_Dev_Tools\Tests\Unit\Arguments;
+
+use PHPUnit\Framework\TestCase;
+use Wp_Dev_Tools\Arguments\ArgumentParser;
+use GetOpt\Option;
+use GetOpt\Operand;
+
+final class ArgumentParser_Test extends TestCase
+{
+    /**
+     * -----------------
+     *   F I X T U R E
+     * -----------------
+     */
+
+    const NARF = 'Narf!';
+    const SCRIPT = 'my-script';
+    const VERSION = '1.0';
+
+    private static string $old_error_log;
+    private static string $log_file;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$old_error_log = ini_get('error_log');
+        self::$log_file = rtrim(sys_get_temp_dir(), '/\\') . '/wp-dev-tools-tests.log';
+        touch(self::$log_file);
+    }
+
+    public function tearDown(): void
+    {
+        file_put_contents(self::$log_file, '');
+        ini_set('error_log', self::$old_error_log);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        file_exists(self::$log_file) && unlink(self::$log_file);
+    }
+
+    /**
+     * ---------------------
+     *   A R G U M E N T S
+     * ---------------------
+     */
+
+    public function test_Can_define_and_retrieve_option(): void
+    {
+        $options = [Option::create('x')];
+        $parser = new ArgumentParser(['options' => $options]);
+        $parser->parse('-x');
+
+        $value = $parser->option('x');
+
+        $this->assertEquals(1, $value);
+    }
+
+    public function test_Can_define_and_retrieve_operand(): void
+    {
+        $operands = [Operand::create('narf', Operand::REQUIRED)];
+        $parser = new ArgumentParser(['operands' => $operands]);
+        $parser->parse(self::NARF);
+
+        $value = $parser->operand('narf');
+
+        $this->assertEquals(self::NARF, $value);
+    }
+
+    /* public function test_Can_define_and_retrieve_command(): void
+    {
+
+    } */
+    
+    /**
+     * ---------------
+     *   E R R O R S
+     * ---------------
+     */
+
+    /**
+     * @depends test_Can_define_and_retrieve_option
+     * @depends test_Can_define_and_retrieve_operand
+     */
+    public function test_Parser_has_no_output_or_error_on_successful_parsing_of_user_defined_arguments(): void
+    {
+        $options = [Option::create('x')];
+        $parser = new ArgumentParser(['options' => $options]);
+
+        $parser->parse('');
+
+        $this->assertFalse($parser->has_output());
+        $this->assertEquals(0, $parser->error_code());
+    }
+
+    /**
+     * @depends test_Parser_has_no_output_or_error_on_successful_parsing_of_user_defined_arguments
+     */
+    public function test_Parse_error_writes_exception_message_to_STDERR(): void
+    {
+        ini_set('error_log', self::$log_file);
+        $operands = [Operand::create('narf', Operand::REQUIRED)];
+        $parser = new ArgumentParser(['operands' => $operands]);
+        
+        $parser->parse('');
+
+        $this->assertStringContainsString('Operand narf is required', file_get_contents(self::$log_file));
+    }
+
+    /**
+     * @depends test_Parse_error_writes_exception_message_to_STDERR
+     */
+    public function test_Missing_argument_yields_error_message_output_and_SYNTAX_ERROR_error_code(): void
+    {
+        $operands = [Operand::create('narf', Operand::REQUIRED)];
+        $parser = new ArgumentParser(['operands' => $operands]);
+        
+        $parser->parse('');
+
+        $this->assertTrue($parser->has_output());
+        $this->assertEquals(ArgumentParser::SYNTAX_ERROR, $parser->error_code());
+    }
+
+    /**
+     * @depends test_Parse_error_writes_exception_message_to_STDERR
+     */
+    public function test_Unexpected_argument_yields_error_message_output_and_SYNTAX_ERROR_error_code(): void
+    {
+        $operands = [Operand::create('narf', Operand::REQUIRED)];
+        $parser = new ArgumentParser(['operands' => $operands]);
+        
+        $parser->parse(self::NARF . ' Zort!');
+
+        $this->assertTrue($parser->has_output());
+        $this->assertEquals(ArgumentParser::SYNTAX_ERROR, $parser->error_code());
+    }
+
+    /**
+     * @depends test_Parse_error_writes_exception_message_to_STDERR
+     */
+    public function test_Invalid_argument_yields_error_message_output_and_INVALID_ARGUMENT_error_code(): void
+    {
+        $operands = [
+            Operand::create('narf', Operand::REQUIRED)->setValidation(function($value) {
+                return $value > 50;
+            })
+        ];
+        $parser = new ArgumentParser(['operands' => $operands]);
+        
+        $parser->parse('42');
+
+        $this->assertTrue($parser->has_output());
+        $this->assertEquals(ArgumentParser::INVALID_ARGUMENT, $parser->error_code());
+    }
+
+    /**
+     * @depends test_Unexpected_argument_yields_error_message_output_and_SYNTAX_ERROR_error_code
+     */
+    public function test_Strict_operands_setting_can_be_overridden_in_constructor(): void
+    {
+        $operands = [Operand::create('narf', Operand::REQUIRED)];
+        $settings = ['strict_operands' => false];
+        $parser = new ArgumentParser(['operands' => $operands], $settings);
+        
+        $parser->parse(self::NARF . ' Zort!');
+
+        $this->assertFalse($parser->has_output());
+        $this->assertEquals(0, $parser->error_code());
+    }
+    
+    /**
+     * -----------------------------
+     *   I N F O   R E Q U E S T S
+     * -----------------------------
+     */
+
+    /**
+     * @depends test_Parser_has_no_output_or_error_on_successful_parsing_of_user_defined_arguments
+     */
+    public function test_Parser_output_is_nonerror_help_text_when_help_option_set(): void
+    {
+        $parser = new ArgumentParser([]);
+
+        $parser->parse('-h');
+
+        $this->assertEquals(0, $parser->error_code());
+        $this->assertTrue($parser->has_output());
+        $this->assertStringContainsString('Usage:', $parser->output());
+    }
+
+    /**
+     * @depends test_Parser_output_is_nonerror_help_text_when_help_option_set
+     */
+    public function test_Parser_output_is_nonerror_version_string_when_version_option_set_and_help_option_not_set(): void
+    {
+        $settings = [
+            'script' => self::SCRIPT,
+            'version' => self::VERSION,
+        ];
+        $parser = new ArgumentParser([], $settings);
+
+        $parser->parse('-v');
+
+        $this->assertEquals(0, $parser->error_code());
+        $this->assertTrue($parser->has_output());
+        $this->assertEquals(sprintf('%s: v%s%s', self::SCRIPT, self::VERSION, PHP_EOL), $parser->output());
+    }
+
+    /**
+     * @depends test_Parser_output_is_nonerror_version_string_when_version_option_set_and_help_option_not_set
+     */
+    public function test_Argument_errors_are_supressed_when_help_option_set(): void
+    {
+        $operands = [Operand::create('narf', Operand::REQUIRED)];
+        $parser = new ArgumentParser(['operands' => $operands]);
+
+        $parser->parse('-h');
+
+        $this->assertEquals(0, $parser->error_code());
+    }
+
+    /**
+     * @depends test_Argument_errors_are_supressed_when_help_option_set
+     */
+    public function test_Argument_errors_are_supressed_when_version_option_set(): void
+    {
+        $operands = [Operand::create('narf', Operand::REQUIRED)];
+        $parser = new ArgumentParser(['operands' => $operands]);
+
+        $parser->parse('-v');
+
+        $this->assertEquals(0, $parser->error_code());
+    }
+}
