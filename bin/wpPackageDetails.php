693b36287ba278d5223447a8a0381cd4da042250
Jason Schousboe <questionmuppet@gmail.com>
2020-11-22 01:58:18 -0600

    Added optional readme and url options to script

--- bin/wpPackageDetails.php
+++ bin/wpPackageDetails.php
@@ -6,6 +6,7 @@
  */
 
 use Wp_Dev_Tools\Arguments\ArgumentParser;
+use GetOpt\GetOpt;
 use GetOpt\Option;
 use GetOpt\Operand;
 use Wp_Dev_Tools\Package_Details\Generators\PluginDetailsGenerator;
@@ -24,19 +25,26 @@ define('VERSION', '1.0-alpha');
  * Script parameters
  */
 $options = [
-    Option::create('p', 'pretty-print'),
+    Option::create('p', 'pretty-print')
+        ->setDescription('Prettify the JSON formatting in the output file.'),
+    
+    Option::create('r', null, GetOpt::REQUIRED_ARGUMENT)
+        ->setArgumentName('readme-file')
+        ->setDescription('Relative path to readme file. Used to generate the "sections" field in the output. This file should adhere to the WordPress plugin readme file standard.')
+        ->setValidation('is_readable', 'Invalid path provided for %s. File "%s" does not exist or cannot be read.'),
+
+    Option::create('u', null, GetOpt::REQUIRED_ARGUMENT)
+        ->setArgumentName('download-url')
+        ->setDescription('Url to package location. Used for the "download_url" field in the output.')
+        ->setValidation(function($url) {
+            return filter_var($url, FILTER_VALIDATE_URL);
+        }, 'Invalid url provided for %s. "%s" does not match a recognized url pattern.'),
 ];
 $operands = [
     Operand::create('source-file', Operand::REQUIRED)
         ->setDescription('Plugin or theme file to extract headers from. Will usually be "plugin-name.php" for plugins and "style.css" for themes.')
         ->setValidation('is_readable', 'Invalid path provided for %s. File "%s" does not exist or cannot be read.'),
 
-    Operand::create('url', Operand::REQUIRED)
-        ->setDescription('Url to location where package can be downloaded.')
-        ->setValidation(function($url) {
-            return filter_var($url, FILTER_VALIDATE_URL);
-        }, 'Invalid url provided for %s. "%s" does not match a recognized url pattern.'),
-    
     Operand::create('output-file', Operand::OPTIONAL)
         ->setDescription('Relative path to output file. When omitted defaults to "package-details.json".')
         ->setDefaultValue('package-details.json'),
@@ -69,9 +77,15 @@ if ($parser->has_output())
  * Write file
  */
 $output = $parser->operand('output-file');
+$prettify = $parser->option('pretty-print');
+$source = new File($parser->operand('source-file'));
+$readme = $parser->option('readme');
+$extras = array_filter([
+    'url' => new Url($parser->option('u')),
+    'readme' => $readme ? new File($readme) : null,
+]);
+
+$generator = new PluginDetailsGenerator($source, $extras);
+
 echo "Writing file to '$output.'";
-$generator = new PluginDetailsGenerator(
-    new File($parser->operand('source-file')),
-    new Url($parser->operand('url'))
-);
-file_put_contents($output, $generator->json($parser->option('pretty-print')));
+file_put_contents($output, $generator->json($prettify));
