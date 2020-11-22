85a0cebb41b9b23402cbd7919bf60c55a7d2ac0b
Jason Schousboe <questionmuppet@gmail.com>
2020-11-21 19:39:05 -0600

    Implemented wpPackageDetails script

---
+++ bin/wpPackageDetails.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * wpPackageDetails
+ * 
+ * Creates a package-details file in JSON format from WordPress plugin and theme files
+ */
+
+use Wp_Dev_Tools\Arguments\ArgumentParser;
+use GetOpt\Option;
+use GetOpt\Operand;
+use Wp_Dev_Tools\Package_Details\Generators\PluginDetailsGenerator;
+use Wp_Dev_Tools\Data\File;
+use Wp_Dev_Tools\Data\Url;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+/**
+ * Script constants
+ */
+define('SCRIPT', 'WP Package Details Generator');
+define('VERSION', '1.0-alpha');
+
+/**
+ * Script parameters
+ */
+$options = [
+    Option::create('p', 'pretty-print'),
+];
+$operands = [
+    Operand::create('source-file', Operand::REQUIRED)
+        ->setDescription('Plugin or theme file to extract headers from. Will usually be "plugin-name.php" for plugins and "style.css" for themes.')
+        ->setValidation('is_readable', 'Invalid path provided for %s. File "%s" does not exist or cannot be read.'),
+
+    Operand::create('url', Operand::REQUIRED)
+        ->setDescription('Url to location where package can be downloaded.')
+        ->setValidation(function($url) {
+            return filter_var($url, FILTER_VALIDATE_URL);
+        }, 'Invalid url provided for %s. "%s" does not match a recognized url pattern.'),
+    
+    Operand::create('output-file', Operand::OPTIONAL)
+        ->setDescription('Relative path to output file. When omitted defaults to "package-details.json".')
+        ->setDefaultValue('package-details.json'),
+];
+
+/**
+ * Parse command-line arguments
+ */
+$args = [
+    'options' => $options,
+    'operands' => $operands,
+];
+$settings = [
+    'script' => SCRIPT,
+    'version' => VERSION,
+];
+$parser = new ArgumentParser($args, $settings);
+$parser->parse();
+
+/**
+ * Exit with error or information request
+ */
+if ($parser->has_output())
+{
+    echo $parser->output();
+    exit($parser->error_code());
+}
+
+/**
+ * Write file
+ */
+$output = $parser->operand('output-file');
+echo "Writing file to '$output.'";
+$generator = new PluginDetailsGenerator(
+    new File($parser->operand('source-file')),
+    new Url($parser->operand('url'))
+);
+file_put_contents($output, $generator->json($parser->option('pretty-print')));
