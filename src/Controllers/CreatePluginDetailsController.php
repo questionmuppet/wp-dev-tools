8557d746f0cc259e99f6784952f03900c84648bf
Jason Schousboe <questionmuppet@gmail.com>
2020-11-23 19:20:56 -0600

    Created CreatePluginDetailsController

---
+++ src/Controllers/CreatePluginDetailsController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * CreatePluginDetailsController
+ * 
+ * Executes a package-details create action for a plugin
+ */
+
+namespace Wp_Dev_Tools\Controllers;
+
+use Wp_Dev_Tools\Package_Details\Generators\PluginDetailsGenerator;
+use Wp_Dev_Tools\Data\File;
+
+final class CreatePluginDetailsController extends CreateDetailsController
+{
+    /**
+     * Create a plugin details generator
+     */
+    protected function generator(File $source, array $extras): PluginDetailsGenerator
+    {
+        return new PluginDetailsGenerator($source, $extras);
+    }
+}
